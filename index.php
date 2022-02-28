763a50f67e9005aa7e62163f9c3530f0ca14f504
ariefr29 <arief.dao29@gmail.com>
2022-02-28 09:01:54 +0700

    add pagination and short profile

--- index.php
+++ index.php
@@ -7,7 +7,17 @@ if (is_search() || is_archive()):
 
 else:
   // display for Index (frontpage)
+  if (get_option('set_profile') == 'yes') {
+    get_template_part( 'template-parts/about' );
+  }
   get_template_part( 'template-parts/content', 'index' );
+
+  the_posts_pagination( array(
+    'mid_size' => 2,
+    'prev_text' => __('Prev'),
+    'next_text' => __('Next'),
+    'screen_reader_text' => '',
+  ));
 endif;
 
 
